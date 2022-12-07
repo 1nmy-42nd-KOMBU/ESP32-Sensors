47c111e486b4f750eb1b292785e5e0aac526ddca
koki0517 <koki1743ok@gmail.com>
2022-12-08 00:29:06 +0900

    arduinoからesp32へのデータ送信成功

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
-TaskHandle_t thp[1];//マルチスレッドのタスクハンドル格納用
-long a = 0;
-
+#include "HardwareSerial.h"
+// this sample code provided by www.programmingboss.com
+#define RXp2 16
+#define TXp2 17
+int8_t list[] = {0,0,0,0};
 void setup() {
-  Serial.begin(115200);
-  xTaskCreatePinnedToCore(Core0a, "Core0a", 4096, NULL, 3, &thp[0], 0); 
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
 }
-
 void loop() {
-  Serial.println(a);//メインで実行するプログラム
-  delay(100);
-}
-
-void Core0a(void *args) {
-  while (1) {
-    delay(1);//サブで実行するプログラムを書く
-    a++;
-  }
+    if (Serial2.available() > 0){
+        char byte_count = 0;
+        Serial.print(Serial2.available());
+        while (Serial2.available() > 0) {
+            list[byte_count] = Serial2.read();
+            byte_count += 1;
+        }
+        Serial.print(list[0]);
+        Serial.print(list[1]);
+        Serial.print(list[2]);
+        Serial.println(list[3]);
+    }
+    delay(10);
 }
