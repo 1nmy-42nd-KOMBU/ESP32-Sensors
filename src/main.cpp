7f926fd69c2e6c2953415d19dfaf3e5a2da51b1e
Koki <89891689+koki0517@users.noreply.github.com>
2023-01-20 16:35:14 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,23 @@
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BNO055.h>
-#include <utility/imumaths.h>
-#include <VL53L1X.h>
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.iodevices import I2CDevice
+from pybricks.parameters import Port
+from pybricks.tools import wait, StopWatch
 
-VL53L1X sensor;
-#define BNO055_SAMPLERATE_DELAY_MS (100)
-#define Wire1_SDA (33)
-#define Wire1_SCL (32)
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28, &Wire1);
+# Initialize the EV3
+ev3 = EV3Brick()
 
-void setup()
-{
-  Serial.begin(115200);
-  Wire1.begin(Wire1_SDA, Wire1_SCL);
-  Wire1.setClock(100000); // use 100 kHz I2C
-  Wire.begin();
+timer = StopWatch()
 
-  sensor.setBus(&Wire1);
-  sensor.setTimeout(500);
-  if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1);
-  }
-  sensor.setDistanceMode(VL53L1X::Long); // long-mode
-  sensor.setMeasurementTimingBudget(50000);
-  sensor.startContinuous(10);
+# Initialize I2C Sensor
+C1 = I2CDevice(Port.S1, 0x04)
 
-  if(!bno.begin())
-  {
-    /* There was a problem detecting the BNO055 ... check your connections */
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
-  }
+wait(100)
 
-  delay(1000);
+start_time = timer.time()
+for i in range(100):
+    tuplea = C1.read(reg=0x04, length=2)
+    print((tuplea[0] << 8) + tuplea[1])
+    wait(100)
 
-  /* Use external crystal for better accuracy */
-  bno.setExtCrystalUse(true);
-}
-
-void loop()
-{
-  // tof-------------------------------------------
-  Serial.print(sensor.read());
-  if (sensor.timeoutOccurred()) { 
-    Serial.print(" TIMEOUT");
-    vlxReset(19);
-  }
-
-  Serial.println();
-
-  // gyro-------------------------------------------
-    sensors_event_t event;
-    bno.getEvent(&event);
-
-  /* The processing sketch expects data as roll, pitch, heading */
-  Serial.print(F("Orientation: "));
-  Serial.print((float)event.orientation.x);
-  Serial.print(F(" "));
-  Serial.print((float)event.orientation.y);
-  Serial.print(F(" "));
-  Serial.print((float)event.orientation.z);
-  Serial.println(F(""));
-
-  delay(BNO055_SAMPLERATE_DELAY_MS);
-}
-
-void vlxReset(uint8_t resetPin) {
-
-  Serial.println("Resetting Sensor");
- /* Stop the Sensor reading and even stop the i2c Transmission */
-  sensor.stopContinuous();
-  Wire.endTransmission();
-
-/* Reset the sensor over here, you can change the delay */
-  digitalWrite(resetPin, LOW);
-  delay(600);  // Change to delay(600); if not using rtos
-  digitalWrite(resetPin, HIGH);
-  delay(600);
-
-
-  if (!sensor.init()) {
-    ESP_LOGE(TAG, "Failed To Detect Sensor.. Restarting!!");
-    ESP.restart();
-  }
-  sensor.setTimeout(500);
-
-  sensor.startContinuous(10);
-}
+print(str((timer.time()-start_time)/100-100))
