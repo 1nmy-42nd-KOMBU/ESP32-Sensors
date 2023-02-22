6328a2936f23d389ed9ab686c6f1b9424862bbd4
Koki <89891689+koki0517@users.noreply.github.com>
2023-02-22 19:05:03 +0900

    もろもろの文法の修正

--- test/Pico.cpp
+++ test/Pico.cpp
@@ -4,6 +4,7 @@
 int num_3digits = 100;
 
 void setup(){
+    Serial.begin(115200);
     //1～10番ピン　デジタル出力へセット
     for (int i=1; i<=10; i++){
         pinMode(i,OUTPUT);
@@ -27,32 +28,37 @@ boolean Num_Array[11][7]={
 
 //LED表示
 void NumPrint(int number){
-    for (int i=1; i<=7; ++){
+    for (int i=1; i<=7; i++){
+        Serial.print(number);
+        Serial.print(",");
+        Serial.print(i-1);
+        Serial.print(",");
+        Serial.println(Num_Array[number][i-1]);
         digitalWrite(i,Num_Array[number][i-1]);
     }
 }
 
 void loop(){
     // 4桁以上だったら999に抑える
-    if num_3digits >= 1000{
+    if (num_3digits >= 1000){
         num_3digits = 999;
     }
 
     // 3桁目
     digitalWrite(10,HIGH);
-    digitalWrite(9.HIGH);
-    digitalwrite(8,LOW);
+    digitalWrite(9,HIGH);
+    digitalWrite(8,LOW);
     NumPrint(num_3digits / 100);
 
     // 2桁目
     digitalWrite(10,HIGH);
-    digitalWrite(9.LOW);
-    digitalwrite(8,HIGH);
+    digitalWrite(9,LOW);
+    digitalWrite(8,HIGH);
     NumPrint(num_3digits % 100 / 10);
 
     // 1桁目
     digitalWrite(10,LOW);
-    digitalWrite(9.HIGH);
-    digitalwrite(8,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(8,HIGH);
     NumPrint(num_3digits % 10);
 }
