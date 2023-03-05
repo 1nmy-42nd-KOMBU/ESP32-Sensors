10625385d30d83fcc62a6f70620f2aec05ef6372
koki0517 <koki1743ok@gmail.com>
2023-03-05 14:53:08 +0900

    ファイル整理

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,84 @@
 #include <Wire.h>
-#include <VL53L1X.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include "HardwareSerial.h"
+// this sample code provided by www.programmingboss.com
+#define RXp2 16
+#define TXp2 17
 
-VL53L1X sensor;
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (100)
 #define Wire1_SDA (33)
 #define Wire1_SCL (32)
 TaskHandle_t thp[1];
-uint16_t tof = 0;
-#define SLAVE_ADDRESS 0x04
 
-int8_t datafromEV3 = 5;
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28, &Wire1);
 
-void setup()
-{
-  Serial.begin(115200);
-  Wire1.begin(Wire1_SDA, Wire1_SCL);
-  Wire1.setClock(100000); // use 100 kHz I2C
-  Wire.begin();
-  Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
+void Core0a(void *args) {//サブCPU(Core0)で実行するプログラム
+  while (1) {//ここで無限ループを作っておく
+    /* Get a new sensor event */
+    sensors_event_t event;
+    bno.getEvent(&event);
 
-  sensor.setBus(&Wire1);
-  sensor.setTimeout(500);
-  if (!sensor.init())
-  {
-    Serial.println("Failed to detect and initialize sensor!");
-    while (1);
+    /* The processing sketch expects data as roll, pitch, heading */
+    Serial.print(F("Orientation: "));
+    Serial.print((float)event.orientation.x);
+    Serial.print(F(" "));
+    Serial.print((float)event.orientation.y);
+    Serial.print(F(" "));
+    Serial.print((float)event.orientation.z);
+    Serial.println(F(""));
+
+    // /* Also send calibration data for each sensor. */
+    // uint8_t sys, gyro, accel, mag = 0;
+    // bno.getCalibration(&sys, &gyro, &accel, &mag);
+    // Serial.print(F("Calibration: "));
+    // Serial.print(sys, DEC);
+    // Serial.print(F(" "));
+    // Serial.print(gyro, DEC);
+    // Serial.print(F(" "));
+    // Serial.print(accel, DEC);
+    // Serial.print(F(" "));
+    // Serial.println(mag, DEC);
+
+    delay(BNO055_SAMPLERATE_DELAY_MS);
   }
-  sensor.setDistanceMode(VL53L1X::Long); // long-mode
-  sensor.setMeasurementTimingBudget(50000);
-  sensor.startContinuous(10);
+}
+
+/**************************************************************************/
+/*
+    Arduino setup function (automatically called at startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+  Wire1.begin(Wire1_SDA, Wire1_SCL);
+  Serial.begin(115200);
+  Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
+  Serial.println("Orientation Sensor Test"); Serial.println("");
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
 
   delay(1000);
+
+  /* Use external crystal for better accuracy */
+  bno.setExtCrystalUse(true);
   xTaskCreatePinnedToCore(Core0a, "Core0a", 4096, NULL, 3, &thp[0], 0); 
 }
 
-void loop(){}
-
-// マスターからのデータ受信イベント
-void receiveEvent(int howMany) {
-  while (Wire.available() > 0) {
-    datafromEV3 = Wire.read();
-    Serial.print(datafromEV3);
-  }
-  Serial.println();
-}
-
-// マスターからのデータ要求イベント
-void requestEvent() {
-  if (datafromEV3 == 5){}
-  else {
-      byte sendtoEV3[2] = {highByte(tof),lowByte(tof)};
-    Wire.write(sendtoEV3,2);
-  }
-}
-
-void Core0a(void *args) {//サブCPU(Core0)で実行するプログラム
-  while (1) {
-    // tof-------------------------------------------
-    tof = sensor.read();
-    // Serial.println(tof);
-    if (sensor.timeoutOccurred()) { 
-      Serial.println(" TIMEOUT");
-      vlxReset(19);
-    }
-  
-    // Serial.println();
-  }
-}
-
-void vlxReset(uint8_t resetPin) {
-
-  Serial.println("Resetting Sensor");
- /* Stop the Sensor reading and even stop the i2c Transmission */
-  sensor.stopContinuous();
-  Wire.endTransmission();
-
-/* Reset the sensor over here, you can change the delay */
-  digitalWrite(resetPin, LOW);
-  delay(600);  // Change to delay(600); if not using rtos
-  digitalWrite(resetPin, HIGH);
-  delay(600);
-
-
-  if (!sensor.init()) {
-    ESP_LOGE(TAG, "Failed To Detect Sensor.. Restarting!!");
-    ESP.restart();
-  }
-  sensor.setTimeout(500);
-
-  sensor.startContinuous(10);
-}
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+void loop(void){}
---
+++ test/pre-main.cpp
@@ -0,0 +1,92 @@
+#include <Wire.h>
+#include <VL53L1X.h>
+
+VL53L1X sensor;
+#define Wire1_SDA (33)
+#define Wire1_SCL (32)
+TaskHandle_t thp[1];
+uint16_t tof = 0;
+#define SLAVE_ADDRESS 0x04
+
+int8_t datafromEV3 = 5;
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire1.begin(Wire1_SDA, Wire1_SCL);
+  Wire1.setClock(100000); // use 100 kHz I2C
+  Wire.begin();
+  Wire.onReceive(receiveEvent);
+  Wire.onRequest(requestEvent);
+
+  sensor.setBus(&Wire1);
+  sensor.setTimeout(500);
+  if (!sensor.init())
+  {
+    Serial.println("Failed to detect and initialize sensor!");
+    while (1);
+  }
+  sensor.setDistanceMode(VL53L1X::Long); // long-mode
+  sensor.setMeasurementTimingBudget(50000);
+  sensor.startContinuous(10);
+
+  delay(1000);
+  xTaskCreatePinnedToCore(Core0a, "Core0a", 4096, NULL, 3, &thp[0], 0); 
+}
+
+void loop(){}
+
+// マスターからのデータ受信イベント
+void receiveEvent(int howMany) {
+  while (Wire.available() > 0) {
+    datafromEV3 = Wire.read();
+    Serial.print(datafromEV3);
+  }
+  Serial.println();
+}
+
+// マスターからのデータ要求イベント
+void requestEvent() {
+  if (datafromEV3 == 5){}
+  else {
+      byte sendtoEV3[2] = {highByte(tof),lowByte(tof)};
+    Wire.write(sendtoEV3,2);
+  }
+}
+
+void Core0a(void *args) {//サブCPU(Core0)で実行するプログラム
+  while (1) {
+    // tof-------------------------------------------
+    tof = sensor.read();
+    // Serial.println(tof);
+    if (sensor.timeoutOccurred()) { 
+      Serial.println(" TIMEOUT");
+      vlxReset(19);
+    }
+  
+    // Serial.println();
+  }
+}
+
+void vlxReset(uint8_t resetPin) {
+
+  Serial.println("Resetting Sensor");
+ /* Stop the Sensor reading and even stop the i2c Transmission */
+  sensor.stopContinuous();
+  Wire.endTransmission();
+
+/* Reset the sensor over here, you can change the delay */
+  digitalWrite(resetPin, LOW);
+  delay(600);  // Change to delay(600); if not using rtos
+  digitalWrite(resetPin, HIGH);
+  delay(600);
+
+
+  if (!sensor.init()) {
+    ESP_LOGE(TAG, "Failed To Detect Sensor.. Restarting!!");
+    ESP.restart();
+  }
+  sensor.setTimeout(500);
+
+  sensor.startContinuous(10);
+}
