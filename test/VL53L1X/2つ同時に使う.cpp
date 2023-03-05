2282d9a528ffeebbae2cad7889cf0c6728db96ed
koki0517 <koki1743ok@gmail.com>
2023-03-05 22:07:37 +0900

    ちゃんと使えるかは知らんけどVLのリスタートを作った

--- test/VL53L1X/2つ同時に使う.cpp
+++ test/VL53L1X/2つ同時に使う.cpp
@@ -53,14 +53,60 @@ void setup() {
   vl_left.startContinuous(50);
 }
 
+void vlxReset()
+{
+  Serial.println("Resetting Sensor");
+ /* Stop the Sensor reading and even stop the i2c Transmission */
+  vl_left.stopContinuous();
+  vl_right.stopContinuous();
+  Wire.endTransmission();
+
+/* Reset the sensor over here, you can change the delay */
+  digitalWrite(xshut_left, LOW);
+  digitalWrite(xshut_right, LOW);
+  delay(600);
+  digitalWrite(xshut_left, HIGH); // 左センサを有効状態に戻す
+  delay(1);
+  vl_left.setTimeout(500); //[msec]
+  if (!vl_left.init())
+  {
+    while (1){
+      Serial.println("Failed to detect and initialize vl_left!");
+      delay(1000);
+    }
+  }
+  vl_left.setAddress(vl_left_address); //2台目センサーのアドレス変更
+  // -------------------------------------------------------------------------------
+
+  digitalWrite(xshut_right, HIGH); //右のVLセンサを有効状態に戻す
+  delay(1);
+  vl_right.setTimeout(500); //[msec]
+  if (!vl_right.init())
+  {
+    while (1){
+      Serial.println("Failed to detect and initialize vl_right!");
+      delay(1000);
+    }
+  }
+
+  vl_right.startContinuous(50);
+  vl_left.startContinuous(50);
+}
+
 void loop() {
   Serial.print("LEFT: ");
   Serial.print(vl_left.read());
-  if (vl_left.timeoutOccurred()) { Serial.print(" TIMEOUT LEFT"); }
+  if (vl_left.timeoutOccurred()) { 
+    Serial.print(" TIMEOUT LEFT");
+    vlxReset()
+  }
 
   Serial.print(", RIGHT: ");
   Serial.print(vl_right.read());
-  if (vl_right.timeoutOccurred()) { Serial.print(" TIMEOUT RIGHT"); }
+  if (vl_right.timeoutOccurred()) {
+    Serial.print(" TIMEOUT RIGHT");
+    vlxReset()
+  }
   Serial.println();
   delay(500);
 }
