cd2291c23337de8a91d161d3a14ab48b83520c47
Koki <89891689+koki0517@users.noreply.github.com>
2023-03-07 21:46:55 +0900

    VL53L0X複数

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,16 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <VL53L1X.h>
+#include <VL53L0X.h>
 
-VL53L1X vl_left;
-VL53L1X vl_right;
+// VL53L0X[0]を左 VL53L0X[1]を右とする
+VL53L0X VL53L0X[2];
 
 const int xshut_left = 23;
 const int xshut_right = 5;
 
 byte vl_left_address = 0x10; // 左のVLのアドレスは0x10
 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin();
-
+void setup(){
   // まず全てのGPIOをLOW
   pinMode(xshut_left, OUTPUT);
   pinMode(xshut_right, OUTPUT);
@@ -21,46 +18,66 @@ void setup() {
   digitalWrite(xshut_right, LOW);
   delay(1);
 
-  digitalWrite(xshut_left, HIGH); // 左センサを有効状態に戻す
+  digitalWrite(xshut_left, HIGH);
   delay(1);
-  vl_left.setTimeout(500); //[msec]
-  if (!vl_left.init())
+  if (!VL53L0X[0].init())
   {
     while (1){
       Serial.println("Failed to detect and initialize vl_left!");
       delay(1000);
     }
   }
-  vl_left.setAddress(vl_left_address); //2台目センサーのアドレス変更
-  // -------------------------------------------------------------------------------
+  VL53L0X[0].setTimeout(500);
+  VL53L0X[0].startContinuous(10);
+  VL53L0X[0].setAddress(vl_left_address);
 
-  digitalWrite(xshut_right, HIGH); //右のVLセンサを有効状態に戻す
+  digitalWrite(xshut_right, HIGH);
   delay(1);
-  vl_right.setTimeout(500); //[msec]
-  if (!vl_right.init())
+  if (!VL53L0X[1].init())
   {
     while (1){
-      Serial.println("Failed to detect and initialize vl_right!");
+      Serial.println("Failed to detect and initialize vl_left!");
       delay(1000);
     }
   }
-
-  vl_right.setDistanceMode(VL53L1X::Long);
-  vl_left.setDistanceMode(VL53L1X::Long);
-  vl_right.setMeasurementTimingBudget(50000); //測定タイミングバジェット(1回の距離測定)に許容される時間[micros]
-  vl_left.setMeasurementTimingBudget(50000); 
-  vl_right.startContinuous(50);
-  vl_left.startContinuous(50);
+  VL53L0X[1].setTimeout(500);
+  VL53L0X[1].startContinuous(10);
 }
 
-void loop() {
-  Serial.print("LEFT: ");
-  Serial.print(vl_left.read());
-  if (vl_left.timeoutOccurred()) { Serial.print(" TIMEOUT LEFT"); }
+void vlxReset()
+{
+  Serial.println("Resetting Sensor");
+ /* Stop the Sensor reading and even stop the i2c Transmission */
+  VL53L0X[0].stopContinuous();
+  VL53L0X[1].stopContinuous();
+  Wire.endTransmission();
 
-  Serial.print(", RIGHT: ");
-  Serial.print(vl_right.read());
-  if (vl_right.timeoutOccurred()) { Serial.print(" TIMEOUT RIGHT"); }
-  Serial.println();
-  delay(500);
+  digitalWrite(xshut_left, LOW);
+  digitalWrite(xshut_right, LOW);
+  delay(1);
+
+  digitalWrite(xshut_left, HIGH);
+  delay(1);
+  if (!VL53L0X[0].init())
+  {
+    while (1){
+      Serial.println("Failed to detect and initialize vl_left!");
+      delay(1000);
+    }
+  }
+  VL53L0X[0].setTimeout(500);
+  VL53L0X[0].startContinuous(10);
+  VL53L0X[0].setAddress(vl_left_address);
+
+  digitalWrite(xshut_right, HIGH);
+  delay(1);
+  if (!VL53L0X[1].init())
+  {
+    while (1){
+      Serial.println("Failed to detect and initialize vl_left!");
+      delay(1000);
+    }
+  }
+  VL53L0X[1].setTimeout(500);
+  VL53L0X[1].startContinuous(10);
 }
